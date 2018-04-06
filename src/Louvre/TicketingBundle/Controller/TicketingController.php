716f44dd3f1e0a6884ce22817ac701909076db50
arthur robert <arthur32810@gmail.com>
2018-04-06 17:10:27 +0200

    Création des entités billet et réservation

--- src/Louvre/TicketingBundle/Controller/TicketingController.php
+++ src/Louvre/TicketingBundle/Controller/TicketingController.php
@@ -6,17 +6,24 @@ use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
 class TicketingController extends Controller
 {
+
     public function indexAction()
     {
          return $this->render('LouvreTicketingBundle:Ticketing:index.html.twig');
     }
-    public function bookingAction(){
+
+    public function bookingAction()
+    {
+          
     }
-    public function prepareAction(){
+
+    public function prepareAction()
+    {
     	
     }
-   public function informationsAction()
-   {
-   		return $this->render('LouvreTicketingBundle:Ticketing:informations.html.twig');
-   }
+
+    public function informationsAction()
+    {
+          return $this->render('LouvreTicketingBundle:Ticketing:informations.html.twig');
+    }
 }
