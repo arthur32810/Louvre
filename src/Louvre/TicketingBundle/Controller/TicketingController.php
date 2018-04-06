11696f31af3172b46e8c166d8ca59c856ef37672
arthur robert <arthur32810@gmail.com>
2018-04-06 15:00:31 +0200

    Création du controlleur

---
+++ src/Louvre/TicketingBundle/Controller/TicketingController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Louvre\TicketingBundle\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+
+class TicketingController extends Controller
+{
+    public function indexAction()
+    {
+         return $this->render('LouvreTicketingBundle:Ticketing:index.html.twig');
+    }
+    public function bookingAction(){
+    }
+    public function prepareAction(){
+    	
+    }
+   public function informationsAction()
+   {
+   		return $this->render('LouvreTicketingBundle:Ticketing:informations.html.twig');
+   }
+}
