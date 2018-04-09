e96b8bf93d62a568fda3d54f71c510534e62690f
arthur robert <arthur32810@gmail.com>
2018-04-09 16:25:30 +0200

    création et appel de la fonction

--- src/Louvre/TicketingBundle/Services/LouvreHourBillet.php
+++ src/Louvre/TicketingBundle/Services/LouvreHourBillet.php
@@ -4,5 +4,15 @@ namespace Louvre\TicketingBundle\Services;
 
 class LouvreHourBillet
 {
-	
+	public function hourBillet($reservation)
+	{
+		$day = new DateTime(date('m/d/Y'));
+	  	$visitday = new DateTime($reservation->getDay()->format('m/d/Y'));
+	 
+		if($visitday == $day)
+		{
+		 	echo "même jour";
+		}
+		else { echo "autre jour";}
+	}
 }
