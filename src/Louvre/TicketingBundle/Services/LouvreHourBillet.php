fccdb7849f9ac23df785788da50f4416a90d8ac6
arthur robert <arthur32810@gmail.com>
2018-04-09 16:32:10 +0200

    modification du service

--- src/Louvre/TicketingBundle/Services/LouvreHourBillet.php
+++ src/Louvre/TicketingBundle/Services/LouvreHourBillet.php
@@ -6,15 +6,18 @@ use DateTime;
 
 class LouvreHourBillet
 {
-	public function hourBillet($reservation, $billet)
+	public function hourBillet($reservation, $billets)
 	{
 		$day = new DateTime(date('m/d/Y'));
 	  	$visitday = new DateTime($reservation->getDay()->format('m/d/Y'));
-	 
-		if($visitday == $day)
-		{
-		 	echo "même jour";
-		}
-		else { echo "autre jour";}
+
+	  	foreach ($billets as $billet) {
+	  		if($visitday == $day)
+			{
+			 	echo "même jour";
+			}
+			else { echo "autre jour";}
+	  	}
+		
 	}
 }
