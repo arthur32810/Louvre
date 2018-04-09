7bd99608cfc2a1cee4c4c0e1521827575df8a599
arthur robert <arthur32810@gmail.com>
2018-04-09 17:41:32 +0200

    développement du service

--- src/Louvre/TicketingBundle/Services/LouvreHourBillet.php
+++ src/Louvre/TicketingBundle/Services/LouvreHourBillet.php
@@ -3,20 +3,53 @@
 namespace Louvre\TicketingBundle\Services;
 
 use DateTime;
+use Symfony\Component\HttpFoundation\RequestStack;
 
 class LouvreHourBillet
 {
+	private $requestStack;
+
+	public function __construct(RequestStack $requestStack)
+	{
+	  	$this->requestStack = $requestStack;
+	}
+
 	public function hourBillet($reservation, $billets)
 	{
+		$request = $this->requestStack->getCurrentRequest();
+
+		//Récupération du jour et du jour de visite
 		$day = new DateTime(date('m/d/Y'));
 	  	$visitday = new DateTime($reservation->getDay()->format('m/d/Y'));
+
+	  	//Défintion d'un compteur pour les billet
+	  	$i=0;
 
 	  	foreach ($billets as $billet) {
 	  		if($visitday == $day)
 			{
-			 	echo "même jour";
+				$i++;
+				
+				//Récupération du type de billet
+				$duration = $billet->getDuration();
+				
+				// Récupération de l'heure 
+				date_default_timezone_set('Europe/Paris');
+				$hour = date('H');
+
+				// Test pour le billet
+				if($hour >= 14 && $duration == 1)
+				{	
+					//Définition de la session
+					$session = $request->getSession();
+
+					//Message d'erreur
+					$session->getFlashBag()->add('hourBillet', 'Votre billet '.$i.' n\'est pas valide. Vous ne pouvez commandé de billet "journée" au-dessus de 14 heures.');
+
+				}
+
 			}
-			else { echo "autre jour";}
+			else { return; }
 	  	}
 		
 	}
