8bdeacf2c186ec3e2af1e78ba2abb2b573a96a66
arthur robert <arthur32810@gmail.com>
2018-04-10 15:38:09 +0200

    developpement du service

--- src/Louvre/TicketingBundle/Services/LouvreQuotaMax.php
+++ src/Louvre/TicketingBundle/Services/LouvreQuotaMax.php
@@ -4,5 +4,33 @@ namespace Louvre\TicketingBundle\Services;
 
 class LouvreQuotaMax
 {
-	
+	private $requestStack;
+	private $em;
+	private $quotaMax;
+
+	public function __construct(RequestStack $requestStack, EntityManagerInterface $em, $quotaMax)
+  {
+    $this->requestStack = $requestStack;
+    $this->em           = $em;
+    $this->quotaMax 	= $quotaMax;
+  }
+
+  public function validate($date, Constraint $constraint)
+  {
+  	// Récupération du quota
+    $quotaMax = $this->quotamax;
+
+    $request = $this->requestStack->getCurrentRequest();
+
+    // on récupére le nombre de réservation pour le jour $date
+    $nReservation = $this->em
+      ->getRepository('LouvreTicketingBundle:Billet')
+      ->nReservation($date);
+  
+    if($nReservation>$quotaMax) { 
+        //erreur
+      }
+
+  }
+
 }
