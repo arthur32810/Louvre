60fa140cc748f11567f0eec80fb10d5a3da0ca60
arthur robert <arthur32810@gmail.com>
2018-04-13 14:12:14 +0200

    solution stripe

--- src/Louvre/TicketingBundle/Controller/TicketingController.php
+++ src/Louvre/TicketingBundle/Controller/TicketingController.php
@@ -72,7 +72,27 @@ class TicketingController extends Controller
 
     public function checkoutAction(Request $request)
     {
-        //TODO : Cette action est à faire
+         \Stripe\Stripe::setApiKey("sk_test_GyKdvxgMo9I1HSjAzeHsdeZp");
+
+        // Get the credit card details submitted by the form
+        $token = $_POST['stripeToken'];
+
+        // Create a charge: this will charge the user's card
+        try {
+            $charge = \Stripe\Charge::create(array(
+                "amount" => 1000, // Amount in cents
+                "currency" => "eur",
+                "source" => $token,
+                "description" => "Paiement Stripe - OpenClassrooms Exemple"
+            ));
+            $this->addFlash("success","Bravo ça marche !");
+            return $this->redirectToRoute("booking_prepare");
+        } catch(\Stripe\Error\Card $e) {
+
+            $this->addFlash("error","Snif ça marche pas :(");
+            return $this->redirectToRoute("booking_prepare");
+            // The card has been declined
+        }
     }
 
     public function informationsAction()
