97e13ff971fc332dfc0463f0955443454ae44401
arthur robert <arthur32810@gmail.com>
2018-04-13 15:37:08 +0200

    développement du service

--- src/Louvre/TicketingBundle/Services/Stripe.php
+++ src/Louvre/TicketingBundle/Services/Stripe.php
@@ -4,5 +4,27 @@ namespace Louvre\TicketingBundle\Services;
 
 class Stripe
 {
-	
+	\Stripe\Stripe::setApiKey("sk_test_GyKdvxgMo9I1HSjAzeHsdeZp");
+
+
+	  // Get the credit card details submitted by the form
+	  $token = $_POST['stripeToken'];
+
+	  // Create a charge: this will charge the user's card
+	  try {
+	      $charge = \Stripe\Charge::create(array(
+	          "amount" => 1000, // Amount in cents
+	          "currency" => "eur",
+	          "source" => $token,
+	          "description" => "Paiement Stripe - OpenClassrooms Exemple"
+	      ));
+
+	      return 'success';
+
+	     
+	  } 
+
+	  catch(\Stripe\Error\Card $e) {
+	  	return 'error'
+	  }
 }
