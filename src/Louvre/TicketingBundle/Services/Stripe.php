605a7634a10341b9501de2537d40591de7202412
arthur robert <arthur32810@gmail.com>
2018-04-13 15:39:24 +0200

    création de la fonction stripe

--- src/Louvre/TicketingBundle/Services/Stripe.php
+++ src/Louvre/TicketingBundle/Services/Stripe.php
@@ -4,27 +4,30 @@ namespace Louvre\TicketingBundle\Services;
 
 class Stripe
 {
-	\Stripe\Stripe::setApiKey("sk_test_GyKdvxgMo9I1HSjAzeHsdeZp");
+	public function stripe()
+	{
+		\Stripe\Stripe::setApiKey("sk_test_GyKdvxgMo9I1HSjAzeHsdeZp");
 
 
-	  // Get the credit card details submitted by the form
-	  $token = $_POST['stripeToken'];
+		  // Get the credit card details submitted by the form
+		  $token = $_POST['stripeToken'];
 
-	  // Create a charge: this will charge the user's card
-	  try {
-	      $charge = \Stripe\Charge::create(array(
-	          "amount" => 1000, // Amount in cents
-	          "currency" => "eur",
-	          "source" => $token,
-	          "description" => "Paiement Stripe - OpenClassrooms Exemple"
-	      ));
+		  // Create a charge: this will charge the user's card
+		  try {
+		      $charge = \Stripe\Charge::create(array(
+		          "amount" => 1000, // Amount in cents
+		          "currency" => "eur",
+		          "source" => $token,
+		          "description" => "Paiement Stripe - OpenClassrooms Exemple"
+		      ));
 
-	      return 'success';
+		      return 'success';
 
-	     
-	  } 
+		     
+		  } 
 
-	  catch(\Stripe\Error\Card $e) {
-	  	return 'error'
-	  }
+		  catch(\Stripe\Error\Card $e) {
+		  	return 'error'
+		  }
+	}
 }
