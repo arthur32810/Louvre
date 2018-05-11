d9fc90e4c865a315ec1dfb704f49375bb65a6835
arthur robert <arthur32810@gmail.com>
2018-05-11 17:03:03 +0200

    test si le code existe déjà

--- src/Louvre/TicketingBundle/Services/LouvreCodeReservation.php
+++ src/Louvre/TicketingBundle/Services/LouvreCodeReservation.php
@@ -2,17 +2,48 @@
 
 namespace Louvre\TicketingBundle\Services;
 
+use Doctrine\ORM\EntityManagerInterface;
+
 class LouvreCodeReservation
 {
+  private $em;
+
+  public function __construct(EntityManagerInterface $em)
+  {
+    $this->em           = $em;
+  }
+
 	function code($car) {
 
-    $code = "";
-    $chaine = "abcdefghijklmnpqrstuvwxy";
-    $chaine .= strtoupper($chaine);
-    $chaine .= "0123456789";
+    $unique_code ='';
 
-    for($i=0; $i<$car; $i++) {
-      $code .= $chaine[rand()%strlen($chaine)];
+    while($unique_code != 'yes')
+    {
+
+      $code = "";
+      $chaine = "abcdefghijklmnpqrstuvwxy";
+      $chaine .= strtoupper($chaine);
+      $chaine .= "0123456789";
+
+      for($i=0; $i<$car; $i++) {
+        $code .= $chaine[rand()%strlen($chaine)];
+      }
+
+      $exist_code = $this->em
+      ->getRepository('LouvreTicketingBundle:Reservation')
+      ->findByReservationCode($code);
+
+      $exist_code = count($exist_code);
+
+      if($exist_code > 0)
+      {
+        $unique_code='no';
+      }
+      else
+      {
+        $unique_code='yes';
+      }
+
     }
 
     return $code;
