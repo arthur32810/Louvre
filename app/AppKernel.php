c23f933e0b05536440b6a6fdfcc0f055ce8b2535
arthur robert <arthur32810@gmail.com>
2018-06-15 17:30:53 +0200

    modified:   app/AppKernel.php 	modified:   app/config/config.yml 	modified:   app/config/parameters.yml.dist 	modified:   app/config/routing.yml 	modified:   app/config/routing_dev.yml 	modified:   app/config/services.yml 	modified:   composer.json 	modified:   composer.lock 	deleted:    src/AppBundle/AppBundle.php 	deleted:    src/AppBundle/Controller/DefaultController.php 	deleted:    tests/AppBundle/Controller/DefaultControllerTest.php 	modified:   var/SymfonyRequirements.php

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -13,15 +13,17 @@ class AppKernel extends Kernel
             new Symfony\Bundle\TwigBundle\TwigBundle(),
             new Symfony\Bundle\MonologBundle\MonologBundle(),
             new Symfony\Bundle\SwiftmailerBundle\SwiftmailerBundle(),
+            new Symfony\Bundle\AsseticBundle\AsseticBundle(),
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
-            new AppBundle\AppBundle(),
+            new Louvre\TicketingBundle\LouvreTicketingBundle(),
         ];
 
         if (in_array($this->getEnvironment(), ['dev', 'test'], true)) {
             $bundles[] = new Symfony\Bundle\DebugBundle\DebugBundle();
             $bundles[] = new Symfony\Bundle\WebProfilerBundle\WebProfilerBundle();
             $bundles[] = new Sensio\Bundle\DistributionBundle\SensioDistributionBundle();
+            $bundles[] = new CoreSphere\ConsoleBundle\CoreSphereConsoleBundle();
 
             if ('dev' === $this->getEnvironment()) {
                 $bundles[] = new Sensio\Bundle\GeneratorBundle\SensioGeneratorBundle();
