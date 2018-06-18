83c9105bb2a04be043e960366aae456002227130
arthur robert <arthur32810@gmail.com>
2018-06-18 13:55:14 +0200

    modification du fichier deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,8 @@
 # config valid for current version and patch releases of Capistrano
 lock "~> 3.11.0"
 
-set :application, "my_app_name"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :application, "Louvre"
+set :repo_url, "artdevelopp@canard:home/artdevelopp/louvre.artdevelopp.fr/git/"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
