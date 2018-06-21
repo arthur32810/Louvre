660098ee10352c704495c6c8b450937d3f36998b
arthur robert <arthur32810@gmail.com>
2018-06-21 17:35:53 +0200

    mofication du fichier app_dev.php

--- web/app_dev.php
+++ web/app_dev.php
@@ -10,12 +10,25 @@ use Symfony\Component\Debug\Debug;
 
 // This check prevents access to debug front controllers that are deployed by accident to production servers.
 // Feel free to remove this, extend it, or make something more sophisticated.
-if (isset($_SERVER['HTTP_CLIENT_IP'])
-    || isset($_SERVER['HTTP_X_FORWARDED_FOR'])
-    || !(in_array(@$_SERVER['REMOTE_ADDR'], ['127.0.0.1', '::1']) || PHP_SAPI === 'cli-server')
+
+function getUserIP() {
+    if( array_key_exists('HTTP_X_FORWARDED_FOR', $_SERVER) && !empty($_SERVER['HTTP_X_FORWARDED_FOR']) ) {
+        if (strpos($_SERVER['HTTP_X_FORWARDED_FOR'], ',')>0) {
+            $addr = explode(",",$_SERVER['HTTP_X_FORWARDED_FOR']);
+            return trim($addr[0]);
+        } else {
+            return $_SERVER['HTTP_X_FORWARDED_FOR'];
+        }
+    }
+    else {
+        return $_SERVER['REMOTE_ADDR'];
+    }
+}
+
+if (!(in_array(@getUserIp(), ['127.0.0.1', '::1', '86.250.194.239', '10.20.0.20']) || PHP_SAPI === 'cli-server')
 ) {
-    header('HTTP/1.0 403 Forbidden');
-    exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
+   header('HTTP/1.0 403 Forbidden');
+   exit('You are not allowed to access this file. Check '.basename(__FILE__).' for more information.');
 }
 
 /** @var \Composer\Autoload\ClassLoader $loader */
